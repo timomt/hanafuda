9e3fc84df75cd1640cd41e73f764bf640d104c40
0ivo0 <164025702+0ivo0@users.noreply.github.com>
2024-10-23 19:12:29 +0200

    Main and test

--- src/test/scala/Card.scala
+++ src/test/scala/Card.scala
@@ -1,5 +1,15 @@
+/*package main.test
 
-
+import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
+import main.scala.Card
 
-//ToDo: Implement the Card Tests
+class CardSpec extends AnyFunSuite with Matchers {
+  test("CardPlant should return correct unicode for each plant") {
+    CardPlant.Pine.unicode should be (" Pine ")
+    CardPlant.Plum.unicode should be (" Plum ")
+    CardPlant.Cherry.unicode should be ("Cherry")
+  }
+}
+
+ */
