74f1560f4707399d6587816d3f9e646304de8b16
timomt <119742190+timomt@users.noreply.github.com>
2024-10-25 11:14:32 +0200

    creating GameManager

--- src/main/scala/Hanafuda.scala
+++ src/main/scala/Hanafuda.scala
@@ -1,10 +1,13 @@
 object Hanafuda {
     @main
     def main(): Unit = {
-        var deck = Deck.defaultDeck()
+        val game = GameManager.newGame()
+        println(game)
+        
+        /*var deck = Deck.defaultDeck()
         val (cardRow, newDeck) = Deck.pollMultiple(deck, 8)
         deck = newDeck
         val lineToPrint = cardRow.map(_.unicode)
-        println(lineToPrint.transpose.map(_.mkString(" ")).mkString("\n") + "\n")
+        println(lineToPrint.transpose.map(_.mkString(" ")).mkString("\n") + "\n")*/
     }
 }
