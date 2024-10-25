f6f9a64f51b565d71b529876dfda9830be6a6d6a
timomt <119742190+timomt@users.noreply.github.com>
2024-10-25 11:14:32 +0200

    creating GameManager

--- src/main/scala/Hanafuda.scala
+++ src/main/scala/Hanafuda.scala
@@ -1,20 +1,13 @@
-import controller.GameController
-import view.TUIManager
-
-/*
-* object Hanafuda
-* entry point of this application.
-* */
-
 object Hanafuda {
     @main
     def main(): Unit = {
-        GameController.add(TUIManager)
-        GameController.processInput("help")
-
-        while (true) {
-            val input = scala.io.StdIn.readLine(s"\n")
-            GameController.processInput(input)
-        }
+        val game = GameManager.newGame()
+        println(game)
+        
+        /*var deck = Deck.defaultDeck()
+        val (cardRow, newDeck) = Deck.pollMultiple(deck, 8)
+        deck = newDeck
+        val lineToPrint = cardRow.map(_.unicode)
+        println(lineToPrint.transpose.map(_.mkString(" ")).mkString("\n") + "\n")*/
     }
 }
