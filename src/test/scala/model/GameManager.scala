634f0d0fbd9ab878940cb36bc6e5315c67e2f75e
0ivo0 <164025702+0ivo0@users.noreply.github.com>
2024-10-31 14:59:13 +0100

    new structure

---
+++ src/test/scala/model/GameManager.scala
@@ -0,0 +1,16 @@
+package model
+
+object GameManager {
+  //ToDo: methode to start a new game
+
+  //ToDo: If a full month has been dealt to the field, the round is void and redealt.
+
+  //ToDo: If there are ¾ of a month dealt to the field, combine the three into one stack, the player who plays the final card of the month gets all of that month
+
+  //ToDo: If either player has a dealt Yaku (hand) the round is ended and they are awarded the points
+  //      "„Yaku“ (eine gewinnbringende Kombination von Karten)"
+  //      Four Hands: 4 cards of the same month: 6pts
+  //      Sticky: 2 cards from 4 different months: 6pts
+
+
+}
