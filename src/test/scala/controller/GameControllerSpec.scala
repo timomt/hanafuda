20a7672d2fb7e94f6a530a1edd975fa41dba4cce
0ivo0 <164025702+0ivo0@users.noreply.github.com>
2024-11-12 16:05:58 +0100

    starting on GameControllerSpec

--- src/test/scala/controller/GameControllerSpec.scala
+++ src/test/scala/controller/GameControllerSpec.scala
@@ -1,62 +1,63 @@
-import controller.GameController
-import org.scalatest.PrivateMethodTester
-import org.scalatest.wordspec.AnyWordSpec
+import model.{Card, CardMonth, CardName, CardType, Deck, GameManager, GameStatePlanned, Player}
+import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
-import model.{Card, CardMonth, CardName, CardType, Deck, GameManager, GameState, Player}
-/*
-class GameControllerSpec extends AnyWordSpec with Matchers with PrivateMethodTester {
-    "matchCards" should {
-        "return error for negative input" in {
-            val matchCards = PrivateMethod[GameState](Symbol("matchCards"))
-            val initialState = GameManager.newGame(" ", " ")
-            val result = GameController.invokePrivate(matchCards(initialState, "-1", "-5"))
-            assert(result.stderr.isDefined)
-        }
-        "call handleMatchPlanned when matchType is PLANNED" in {
+import controller.GameController
+import view.TUIManager.printHelp
 
-        }
-        "call handleMatchRandom when matchType is RANDOM" in {
 
-        }
+class GameControllerSpec extends AnyFlatSpec with Matchers {
+
+  "new Game" should "initialize a new game and notify observer" in {
+    GameController.newGame("Test1", "Test2")
+    GameController.gameState match {
+      case Some(GameStatePlanned(players, _, _, _, _)) =>
+        players.map(_.name) should contain allOf ("Test1", "Test2")
+      case _ => fail("GameState was not initialized correctly")
     }
-    "handleMatchRandomMatch" should {
-        "return error for invalid match" in {
-            val method = PrivateMethod[GameState](Symbol("handleMatchRandomMatch"))
-            val initialState = GameManager.newGame(" ", " ").copy(board = Deck(List(Card(CardMonth.JANUARY, CardType.HIKARI, CardName.PLAIN))),
-                queued = Some(Card(CardMonth.MARCH, CardType.HIKARI, CardName.PLAIN)))
-            val result = GameController.invokePrivate(method(initialState, 1))
-            assert(result.stderr.isDefined)
-        }
-        "collect a whole month properly" in {
+  }
 
-        }
-        "execute default match properly" in {
-            val method = PrivateMethod[GameState](Symbol("handleMatchRandomMatch"))
-            val initialState = GameState(
-                players = List(Player(" ", Deck.defaultDeck(), Deck(List.empty), 0),
-                    Player(" ", Deck.defaultDeck(), Deck(List.empty), 0)),
-                deck = Deck.defaultDeck(),
-                board = Deck(List(Card(CardMonth.MARCH, CardType.KASU, CardName.PLAIN))),
-                matched = Deck(List(Card(CardMonth.JULY, CardType.HIKARI, CardName.PLAIN),
-                    Card(CardMonth.MAY, CardType.HIKARI, CardName.PLAIN))),
-                matchType = MatchType.RANDOM,
-                queued = Some(Card(CardMonth.MARCH, CardType.HIKARI, CardName.PLAIN)),
-                stdout = None,
-                stderr = None
-            )
-            val result = GameController.invokePrivate(method(initialState, 1))
-            assert(result.stderr.isEmpty)
-            assert(result.stdout.isDefined)
-            assert(result.queued.isEmpty)
-            assert(result.matchType === MatchType.PLANNED)
-            assert(result.matched.cards.isEmpty)
-            assert(result.board.cards.isEmpty)
-            assert(result.players(1).side.cards === List(
-                Card(CardMonth.MARCH, CardType.HIKARI, CardName.PLAIN),
-                Card(CardMonth.MARCH, CardType.KASU, CardName.PLAIN),
-                Card(CardMonth.JULY, CardType.HIKARI, CardName.PLAIN),
-                Card(CardMonth.MAY, CardType.HIKARI, CardName.PLAIN)
-            ))
-        }
+  "processInput" should "print help" in {
+    val stream = new java.io.ByteArrayOutputStream()
+    Console.withOut(stream) {
+      GameController.processInput("help")
     }
-}*/
+    val output = stream.toString
+    output should equal(printHelp())
+  }
+    it should "exit the game" in {
+      assertThrows[RuntimeException](GameController.processInput("exit"))
+    }
+    /*
+    it should "print error message" in {
+      val stream = new java.io.ByteArrayOutputStream()
+      Console.withOut(stream) {
+        GameController.processInput("start")
+      }
+      val output = stream.toString
+      output should equal("[Error]: You submitted a command that requires a started game without starting it correctly.\n")
+    }
+     */
+  it should "print an error message when input is invalid and gameState is empty" in {
+    GameController.gameState = None
+    val stream = new java.io.ByteArrayOutputStream()
+    Console.withOut(stream) {
+      GameController.processInput("invalid command")
+    }
+    val output = stream.toString
+    output should equal("[Error]: You submitted a command that requires a started game without starting it correctly.\n")
+  }
+
+    it should "continue the game" in {
+      GameController.newGame("Test1", "Test2")
+      val stream = new java.io.ByteArrayOutputStream()
+      Console.withOut(stream) {
+        GameController.processInput("continue")
+      }
+      val output = stream.toString
+      output should equal("")
+    }
+
+    it should "match the cards" in {
+
+    }
+}
