e658b4f5c8432845e51cfe04b9466d1588f6c68d
0ivo0 <164025702+0ivo0@users.noreply.github.com>
2024-11-15 08:20:40 +0100

    GameControllerSpec Test Update

--- src/test/scala/controller/GameControllerSpec.scala
+++ src/test/scala/controller/GameControllerSpec.scala
@@ -1,10 +1,42 @@
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import controller.GameController
-import model.{DisplayType, GameManager, GameState}
+import model.{Card, CardMonth, CardName, CardType, Deck, DisplayType, GameManager, GameState, GameStateRandom, Player}
 
 class GameControllerSpec extends AnyFlatSpec with Matchers {
 
+  val player1Deck = Deck(List(
+    Deck.defaultDeck().cards.head,
+    Deck.defaultDeck().cards(1),
+    Deck.defaultDeck().cards(2),
+    Deck.defaultDeck().cards(3),
+    Deck.defaultDeck().cards(4),
+    Deck.defaultDeck().cards(5),
+    Deck.defaultDeck().cards(6),
+    Deck.defaultDeck().cards(7)
+  ))
+
+  val player2Deck = Deck(List(
+    Deck.defaultDeck().cards(8),
+    Deck.defaultDeck().cards(9),
+    Deck.defaultDeck().cards(10),
+    Deck.defaultDeck().cards(11),
+    Deck.defaultDeck().cards(12),
+    Deck.defaultDeck().cards(13),
+    Deck.defaultDeck().cards(14),
+    Deck.defaultDeck().cards(15)
+  ))
+
+  val tableDeck = Deck(List(
+    Deck.defaultDeck().cards(16),
+    Deck.defaultDeck().cards(17),
+    Deck.defaultDeck().cards(18),
+    Deck.defaultDeck().cards(19),
+    Deck.defaultDeck().cards(20),
+    Deck.defaultDeck().cards(21),
+    Deck.defaultDeck().cards(22),
+    Deck.defaultDeck().cards(23)
+  ))
 
   "A GameController" should "initialize a new game" in {
     GameController.newGame("Player1", "Player2")
@@ -36,27 +68,115 @@ class GameControllerSpec extends AnyFlatSpec with Matchers {
   }
 
   it should "process input 'match 1 2'" in {
-    GameController.newGame("Player1", "Player2")
+    val game = GameStateRandom(
+      players = List(
+        Player(
+          name = "",
+          hand = Deck(player2Deck.cards),
+          side = Deck(List.empty),
+          score = 0
+        ),
+        Player(
+          name = "",
+          hand = Deck(player2Deck.cards),
+          side = Deck(List.empty),
+          score = 0
+        )
+      ),
+      deck = Deck.defaultDeck(),
+      board = Deck(List(Card(CardMonth.JULY, CardType.HIKARI, CardName.PLAIN))),
+      matched = Deck(List.empty),
+      queued = Card(CardMonth.JULY, CardType.HIKARI, CardName.PLAIN),
+      stdout = None,
+      stderr = None
+    )
+    GameController.gameState = Some(game)
     GameController.processInput("match 1 2")
     GameController.gameState.get.players.head.hand.cards should have size 8
     GameController.gameState.get.players(1).hand.cards should have size 8
   }
 
   it should "process input 'match 1'" in {
-    GameController.newGame("Player1", "Player2")
+    val game = GameStateRandom(
+      players = List(
+        Player(
+          name = "",
+          hand = Deck(player2Deck.cards),
+          side = Deck(List.empty),
+          score = 0
+        ),
+        Player(
+          name = "",
+          hand = Deck(player2Deck.cards),
+          side = Deck(List.empty),
+          score = 0
+        )
+      ),
+      deck = Deck.defaultDeck(),
+      board = Deck(List(Card(CardMonth.JULY, CardType.HIKARI, CardName.PLAIN))),
+      matched = Deck(List.empty),
+      queued = Card(CardMonth.JULY, CardType.HIKARI, CardName.PLAIN),
+      stdout = None,
+      stderr = None
+    )
+    GameController.gameState = Some(game)
     GameController.processInput("match 1")
     GameController.gameState.get.players.head.hand.cards should have size 8
     GameController.gameState.get.players(1).hand.cards should have size 8
   }
 
   it should "process input 'discard 1'" in {
-    GameController.newGame("Player1", "Player2")
+    val game = GameStateRandom(
+      players = List(
+        Player(
+          name = "",
+          hand = Deck(player2Deck.cards),
+          side = Deck(List.empty),
+          score = 0
+        ),
+        Player(
+          name = "",
+          hand = Deck(player2Deck.cards),
+          side = Deck(List.empty),
+          score = 0
+        )
+      ),
+      deck = Deck.defaultDeck(),
+      board = Deck(List(Card(CardMonth.JULY, CardType.HIKARI, CardName.PLAIN))),
+      matched = Deck(List.empty),
+      queued = Card(CardMonth.JULY, CardType.HIKARI, CardName.PLAIN),
+      stdout = None,
+      stderr = None
+    )
+    GameController.gameState = Some(game)
     GameController.processInput("discard 1")
     GameController.gameState.get.players.head.hand.cards should have size 8
   }
 
   it should "process input 'discard'" in {
-    GameController.newGame("Player1", "Player2")
+    val game = GameStateRandom(
+      players = List(
+        Player(
+          name = "",
+          hand = Deck(player2Deck.cards),
+          side = Deck(List.empty),
+          score = 0
+        ),
+        Player(
+          name = "",
+          hand = Deck(player2Deck.cards),
+          side = Deck(List.empty),
+          score = 0
+        )
+      ),
+      deck = Deck.defaultDeck(),
+      board = Deck(List(Card(CardMonth.JULY, CardType.HIKARI, CardName.PLAIN))),
+      matched = Deck(List.empty),
+      queued = Card(CardMonth.JULY, CardType.HIKARI, CardName.PLAIN),
+      stdout = None,
+      stderr = None
+    )
+    GameController.gameState = Some(game)
     GameController.processInput("discard")
     GameController.gameState.get.players.head.hand.cards should have size 8
   }
